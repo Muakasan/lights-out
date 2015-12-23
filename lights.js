87e709fd6023f73149db29e1819e21394c7ee6bf
muakasan <muakasan@gmail.com>
2015-12-23 10:18:41 -0500

    finished?

---
+++ lights.js
@@ -0,0 +1,20 @@
+var lightOn = false;
+$("#lights-on-img").hide();
+
+$("#switch-img").on("touchstart click", function(){
+    if(lightOn)
+    {
+        $(this).addClass("rotate180");
+        $("#lights-label").text("Light's Off!");
+        $("#lights-on-img").hide();
+        $("#lights-off-img").show();
+    }
+    else
+    {
+        $(this).removeClass("rotate180");        
+        $("#lights-label").text("Light's On!");
+        $("#lights-off-img").hide();
+        $("#lights-on-img").show();
+    }
+    lightOn = !lightOn;
+});
